ca7e6cd4ee443c3f83fce2dd580fe9a2b219f6b3
Trentendo <trentendo@outlook.com>
2021-09-29 09:12:05 +1000

    fixed pause menu problem

--- GAM20003-Project/Assets/Scripts/Menus/PauseMenu.cs
+++ GAM20003-Project/Assets/Scripts/Menus/PauseMenu.cs
@@ -13,7 +13,7 @@ public class PauseMenu : MonoBehaviour
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Escape))
+        if(Input.GetKeyDown(KeyCode.Escape)|| Input.GetKeyDown("joystick button 7"))
         {
             if(isPaused)
             {
