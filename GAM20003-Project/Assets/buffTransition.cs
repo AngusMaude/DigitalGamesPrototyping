7b23f93176ceed2b8311732597babc013d430d27
Trentendo <trentendo@outlook.com>
2021-10-21 14:40:00 +1100

    optimised menu and build settings

--- GAM20003-Project/Assets/buffTransition.cs
+++ GAM20003-Project/Assets/buffTransition.cs
@@ -9,12 +9,7 @@ public class buffTransition : MonoBehaviour {
 
     public void OnMouseDown()
     {
-<<<<<<< Updated upstream
         gameObject.GetComponent<HealthBuff>().ApplyBuff();
-=======
-        gameObject.GetComponent<BuffHandler>ApplyBuff();
->>>>>>> Stashed changes
-        SceneManager.LoadScene("Map2");
     }
 
 }
